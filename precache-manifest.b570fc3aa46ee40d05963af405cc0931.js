00f7eaea1ff0638fc8678c5e5c39dd1841309d78
Th3.C0d3r-418 = Rafael Luis <rafaellfra@hotmail.com>
2020-06-25 16:18:23 -0300

    Deploy React Salario

---
+++ precache-manifest.b570fc3aa46ee40d05963af405cc0931.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c792594c5d981f796bf5df4fdcd287b",
+    "url": "/React-Salario/index.html"
+  },
+  {
+    "revision": "3ccc6325e5533b2af67b",
+    "url": "/React-Salario/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "0cc393e8598786aa1b4e",
+    "url": "/React-Salario/static/css/main.113707a3.chunk.css"
+  },
+  {
+    "revision": "3ccc6325e5533b2af67b",
+    "url": "/React-Salario/static/js/2.9177dd73.chunk.js"
+  },
+  {
+    "revision": "617cffb371217e96e8833c60b967b3ba",
+    "url": "/React-Salario/static/js/2.9177dd73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cc393e8598786aa1b4e",
+    "url": "/React-Salario/static/js/main.c0df364d.chunk.js"
+  },
+  {
+    "revision": "019cf2b195b4df8c497b",
+    "url": "/React-Salario/static/js/runtime-main.f0a1db14.js"
+  }
+]);
